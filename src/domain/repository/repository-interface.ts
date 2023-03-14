366e0fa9a69a97d1e9f144a5e443fa8f7b28a6a2
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-03-14 09:57:27 -0300

    feat: creating product repository  infrastructure

--- src/domain/repository/repository-interface.ts
+++
@@ -1,6 +0,0 @@
-export default interface RepositoryInterface<T> {
-  create(entity: T): Promise<void>;
-  update(entity: T): Promise<void>;
-  findAll(): Promise<T[]>;
-  find(id: string): Promise<T>;
-}
