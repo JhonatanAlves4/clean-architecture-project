8610be5af79009b01183261518bfe14de9c65c5c
Jhonatan Alves <jhonatan4alves@gmail.com>
2023-09-28 21:35:46 -0300

    feat: product notification pattern

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -4,19 +4,27 @@ describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
       const product = new Product("", "Product 1", 90);
-    }).toThrowError("Id is required");
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("1", "", 90);
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("1", "Product 1", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("1", "", -90);
+    }).toThrowError(
+      "product: Name is required,product: Price must be greater than zero"
+    );
   });
 
   it("should change name", () => {
